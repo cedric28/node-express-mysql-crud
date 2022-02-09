386696b193852945171f48da1e3591a338474423
Cedric Legarda <cedric_legarda28@yahoo.com>
2022-02-10 04:38:01 +0800

    cors

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const error = require("./middleware/error");
+const cors = require("cors");
 const app = express();
 
 /* Middleware */
 app.use(express.json())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-
+app.use(cors());
 /* Routes */
 const users = require("./routes/users");
 app.use("/api/users", users);
